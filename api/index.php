03d3a02c3d7aa687239292c06bb9aa88b7c470b4
Piero <pierosalutox92@gmail.com>
2017-07-28 12:32:38 +0200

    file di prova

--- api/index.php
+++ api/index.php
@@ -5,3 +5,9 @@ $csv = array_map('str_getcsv', file(__DIR__.'/../data/dati.csv'));
 echo '<pre>';
 var_dump($csv);
 echo '</pre>';
+
+$csv=array_map("str_getcsv", file(__DIR__.'/../data/prova.csv'));
+
+echo '<pre>';
+var_dump($csv); //stampa contenuto
+echo '</pre>';
